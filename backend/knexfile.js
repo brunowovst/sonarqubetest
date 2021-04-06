ef748b64303a6909a642c03f7a38d8fd2edacf42
Bruno Wovst <brunowovst@gmail.com>
2021-04-06 11:20:14 -0300

    criando project sonarqubetest

---
+++ backend/knexfile.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite'
+    },
+    migrations:{
+      directory:'./src/database/migrations'
+    },
+    useNullAsDefault:true
+  },
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/test.sqlite'
+    },
+    migrations:{
+      directory:'./src/database/migrations'
+    },
+    useNullAsDefault:true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE,
+      user:     process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory:'./src/database/migrations'
+    }
+  }
+
+};
